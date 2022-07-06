0715b95b44d8b240dba01affbc260d747f2e53ac
GuilhermeBraz <gvcerveirabraz@gmail.com>
2022-07-06 15:44:22 -0300

    add script
    Co-authored-by: RafaelBP02 <50378927+RafaelBP02@users.noreply.github.com>

---
+++ source/script.py
@@ -0,0 +1,89 @@
+import networkx
+import matplotlib.pyplot as plt
+import random
+
+
+def dijkstra_path(graph, start, end):
+    """
+    Finds the shortest path between two nodes in a graph using Dijkstra's algorithm.
+    :param graph: The graph to search.
+    :param start: The node to start from.
+    :param end: The node to end at.
+    :return: A list of nodes in the shortest path.
+    """
+    # Initialise the distance dictionary.
+    distance = {}
+    for node in graph.nodes():
+        distance[node] = float('inf')
+    distance[start] = 0
+
+    # Initialise the predecessor dictionary.
+    predecessor = {}
+    for node in graph.nodes():
+        predecessor[node] = None
+
+    # Initialise the queue.
+    queue = []
+    for node in graph.nodes():
+        queue.append(node)
+
+    # While the queue is not empty.
+    while queue:
+        # Get the node with the lowest distance.
+        current = min(queue, key=lambda x: distance[x])
+        # If the current node is the end node, return the path.
+        if current == end:
+            path = []
+            while predecessor[current]:
+                path.append(current)
+                current = predecessor[current]
+            path.append(current)
+            return path[::-1]
+        # Remove the current node from the queue.
+        queue.remove(current)
+        # For each neighbour of the current node.
+        for neighbour in graph.neighbors(current):
+            # If the distance to the neighbour is lower than the current distance.
+            if distance[neighbour] > distance[current] + graph[current][neighbour]['weight']:
+                # Update the distance to the neighbour.
+                distance[neighbour] = distance[current] + \
+                    graph[current][neighbour]['weight']
+                # Update the predecessor of the neighbour.
+                predecessor[neighbour] = current
+    # If no path was found, return None.
+    return None
+
+
+nodes = [
+    'RO', 'MT', 'GO', 'AM', 'RR', 'CE', 'RN',
+    'AC', 'MS', 'TO', 'MG', 'AP', 'PA', 'MA', 'PB_JPA',
+    'RA', 'PR', 'BA', 'SE', 'AL', 'PI',
+    'SC', 'SP', 'RJ', 'ES', 'PE', 'PB-CGE'
+]
+
+edges = [('DF', 'RO'), ('RO', 'MT'), ('RO', 'AP'), ('AP', 'AL'),
+         ('AL', 'SP'), ('MT', 'SP'), ('MT', 'RJ')]
+
+# Create a graph with the nodes and edges
+graph = networkx.Graph()
+graph.add_nodes_from(nodes)
+graph.add_edges_from(edges)
+
+# add weights to the edges
+for edge in graph.edges():
+    graph.add_edge(edge[0], edge[1], weight=random.randint(1, 10))
+
+# add weight 100 to edge (RO, MT)
+graph.add_edge('RO', 'MT', weight=100)
+
+# get the shortest path between two nodes using dijkstra's algorithm
+# path = networkx.dijkstra_path(graph, 'DF', 'SP')
+path = dijkstra_path(graph, 'DF', 'SP')
+
+print(path)
+
+
+# get edges weights
+weights = [graph[u][v]['weight'] for u, v in graph.edges()]
+networkx.draw(graph, width=weights, with_labels=True)
+plt.show()
