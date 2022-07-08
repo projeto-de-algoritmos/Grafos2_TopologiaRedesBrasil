3e671c754c5b69d1c69c508739c13c25ab8f76a2
RafaelBP02 <rafaelbertopereira@gmail.com>
2022-07-08 17:52:00 -0300

    Tornando os nós do grafo fixos e destacando o melhor caminho de cor verde
    Co-authored-by: Guilherme Braz <gvcerveirabraz@gmail.com>

--- source/script.py
+++ source/script.py
@@ -55,13 +55,19 @@ def dijkstra_path(graph, start, end):
 
 
 nodes = [
-    'RO', 'MT', 'GO', 'AM', 'RR', 'CE', 'RN',
-    'AC', 'MS', 'TO', 'MG', 'AP', 'PA', 'MA', 
+    'RO', 'MT', 'DF', 'GO', 'AM', 'RR', 'CE', 'RN',
+    'AC', 'MS', 'TO', 'MG', 'AP', 'PA', 'MA', 'RS', 
     'PB_JPA', 'PR', 'BA', 'SE', 'AL', 'PI',
     'SC', 'SP', 'RJ', 'ES', 'PE', 'PB_CGE'
 ]
+fixed_positions = {
+    'RO':(5,5), 'MT':(4,4), 'DF':(0,0), 'GO':(3,3), 'AM':(6,6), 'RR':(3,7), 'CE':(8,8), 'RN':(7,3),
+    'AC':(7,1), 'MS':(5,4), 'TO':(2,4), 'MG':(1,5), 'AP':(3,5), 'PA':(2,4), 'MA':(5,8), 
+    'PB_JPA':(9,3), 'PR':(4,5), 'BA':(2,6), 'SE':(5,7), 'AL':(5,3), 'RS':(3,7), 'PI':(10,4),
+    'SC':(0,7), 'SP':(0, -2), 'RJ':(5,7), 'ES':(0,4), 'PE':(4,6), 'PB_CGE':(5,5)
+                   }
 
-edges = edges = [("DF", "TO"), ("DF", "GO"), ("DF", "AC"), ("DF", "RJ"), ("DF", "MG"),
+edges = [("DF", "TO"), ("DF", "GO"), ("DF", "AC"), ("DF", "RJ"), ("DF", "MG"),
 ("DF", "MA"), ("DF", "CE"), ("DF", "AM"), ("DF", "SP"), ("GO", "MT"), ("GO", "TO"), 
 ("TO", "PA"), ("MT", "MS"), ("MT", "RO"), ("RO", "AC"), ("MS", "PR"), ("PR", "RS"), ("RS", "SC"), ("SC", "SP"), ("SP", "PR"), ("RS", "SP"), ("PR", "SC"), ("SP", "RJ"), ("SP", "RJ"), ("SP", "MG"), ("SP", "CE"), ("RJ", "MG"), ("RJ", "ES"), ("ES", "BA"), ("MG", "BA"), ("BA", "CE"), ("BA", "SE"), ("SE", "AL"), ("AL", "PE"), ("PA", "MA"),
 ("BA", "PB_CGE"), ("PE", "PI"), ("PI", "MA"), ("AM", "RR"), ("RR", "CE"), ("CE", "RN"),
@@ -84,10 +90,17 @@ graph.add_edge('RO', 'MT', weight=100)
 # path = networkx.dijkstra_path(graph, 'DF', 'SP')
 path = dijkstra_path(graph, 'DF', 'SP')
 
+color_map = []
+for node in graph:
+    if node not in path:
+        color_map.append('blue')
+    else: 
+        color_map.append('green')      
+
 print(path)
 
 
 # get edges weights
 weights = [graph[u][v]['weight'] for u, v in graph.edges()]
-networkx.draw(graph, width=weights, with_labels=True)
+networkx.draw(graph, node_size=800, node_color=color_map, pos=fixed_positions, with_labels=True, linewidths=10)
 plt.show()
